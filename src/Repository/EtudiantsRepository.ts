53b3436c2879a87054b6ce476506d5ae1dbf965a
zoandria-gif <zo@mail.hei.school>
2026-08-13 21:11:13 +0300

    Repository: etudiantsRepository fait

---
+++ src/Repository/EtudiantsRepository.ts
@@ -0,0 +1,11 @@
+import { pool } from "../../db.js";
+
+export async function findAll() {
+  const result = await pool.query("SELECT * FROM etudiants");
+  return result.rows;
+}
+
+export async function findById(id: string) {
+  const result = await pool.query("SELECT * FROM etudiants WHERE id = $1", [id]);
+  return result.rows[0]; // un seul étudiant, ou undefined si pas trouvé
+}
