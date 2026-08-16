d50bbc5191d62729e02a13865227d35aa7662a81
zoandria-gif <zo@mail.hei.school>
2026-08-16 22:00:06 +0300

    feat: authMiddleware added into Projet.ts

--- Projet.ts
+++ Projet.ts
@@ -1,13 +1,17 @@
 import express from "express";
 import * as studentsController from "./src/controller/studentsController.js"
+import { authMiddleware } from "./src/middleware/authMiddleware.js";
 
 const app = express();/*App typé-name*/
 app.use(express.json());/*App typé-name*/
+
 const port = 3000;
 
-app.get("/students", studentsController.getAllStudents);
-app.get("/students/:id", studentsController.getStudentsById);
+app.get("/students",authMiddleware, studentsController.getAllStudents);
+app.get("/students/:id", authMiddleware, studentsController.getStudentsById);
+
 app.post("/students/login", studentsController.loginStudent);
+
 
 app.listen(port, () => {
   console.log(`Serveur lancé sur http://localhost:${port}`);
