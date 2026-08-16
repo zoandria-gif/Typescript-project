ca32a18471c7f110ec94871724317a3db5f2e414
zoandria-gif <zo@mail.hei.school>
2026-08-16 20:21:43 +0300

    feat:autMiddleware created

---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from "express";
+import { verifyToken } from "../utils/jwt.js";
+
+export interface AuthRequest extends Request {
+    studentId?: number;
+}
+
+export function authMiddleware(req: AuthRequest, res: Response, next: NextFunction) {
+    const token = req.header("authorization")?.replace("Bearer ", "");
+    if (!token) {
+        return res.status(401).json({ message: "No token provided" });
+    }
+    const decoded = verifyToken(token);
+    if (!decoded) {
+        return res.status(401).json({ message: "Invalid token" });
+    }
+    req.studentId = (decoded as { id: number }).id;
+    next();
+
+}
