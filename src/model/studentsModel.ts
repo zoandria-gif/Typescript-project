841c4981768e0f0a6994987b81425f0caf776d67
zoandria-gif <zo@mail.hei.school>
2026-08-16 15:26:12 +0300

    password added to a model studentsModel

--- src/model/studentsModel.ts
+++ src/model/studentsModel.ts
@@ -3,11 +3,13 @@ export class student {
   firstName: string;
   lastName: string;
   number: string;
+  password: string;
 
-  constructor(id: number, firstName: string, lastName: string, number: string) {
+  constructor(id: number, firstName: string, lastName: string, number: string, password: string) {
     this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
     this.number = number;
+    this.password = password; // Initialize password as an empty string
   }
 }
